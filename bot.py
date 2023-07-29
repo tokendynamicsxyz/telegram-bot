8dd28b8ce222d2cc09d8ba4e8c71e0d9935773fc
FL <fl@FLs-MacBook-Pro.local>
2023-07-29 15:05:39 -0400

    keys working

--- bot.py
+++ bot.py
@@ -18,6 +18,11 @@ bot.
 import logging
 import json
 
+###########################
+# For serving resource list
+from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler, CallbackContext
+
 from telegram import __version__ as TG_VER
 
 try:
@@ -64,6 +69,38 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
+################################################
+# Resource list (keyword: file URL or file path)
+resources = {
+    "keyword1": "https://example.com/file1.pdf",
+    "keyword2": "https://example.com/file2.txt",
+    # Add more keywords and corresponding file URLs or file paths as needed
+}
+
+# Define the /resources command handler
+async def resources_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message containing the available resources with hyperlinks."""
+    keyboard = [
+        [InlineKeyboardButton(keyword, callback_data=keyword) for keyword in resources.keys()]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Resources:\n",
+        reply_markup=reply_markup
+    )
+
+# Define the callback query handler to handle resource button clicks
+async def handle_resource_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    resource_name = query.data
+    if resource_name in resources:
+        file_url = resources[resource_name]
+        await query.message.reply_text(
+            f"Here is the file for resource '{resource_name}': {file_url}"
+        )
+    else:
+        await query.message.reply_text("Resource not found. Please click on a valid resource button.")
 
 def main() -> None:
     """Start the bot."""
@@ -75,12 +112,11 @@ def main() -> None:
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(token).build()
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    # Add command handler for /resources command
+    application.add_handler(CommandHandler("resources", resources_command))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    # Add callback query handler for resource buttons
+    application.add_handler(CallbackQueryHandler(handle_resource_button))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
